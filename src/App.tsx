5e808da5a391a6b0f8c238f86eb0740edfb0b44f
bkdev98 <bkdev98@gmail.com>
2021-02-12 21:45:00 +0700

    polar power

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import 'codemirror/lib/codemirror.css';
 import 'polar-tui-editor/dist/toastui-editor.css';
 
@@ -7,14 +7,23 @@ import { Editor } from 'polar-tui-editor-react';
 import './App.css';
 
 function App() {
+  const editorRef = useRef(null);
+
+  function handleChange() {
+    const value = (editorRef.current as any).getInstance().getMarkdown();
+    localStorage.setItem('polar', value || '');
+  }
+
   return (
     <div className="App">
       <Editor
-        initialValue={`# A wonderful new note\nKeep calm and write something`}
+        ref={editorRef}
+        initialValue={localStorage.getItem('polar') || `# A wonderful new note\nKeep calm and write something`}
         height="100vh"
         initialEditType="markdown"
         useCommandShortcut={true}
         hideModeSwitch
+        onChange={handleChange}
         toolbarItems={[
           'heading',
           'bold',
